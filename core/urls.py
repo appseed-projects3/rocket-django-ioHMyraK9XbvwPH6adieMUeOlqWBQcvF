7b141aac0b24ada771969c988e9264f09c8cf121
AppSeed Generated Projects <108474994+appseed-projects3@users.noreply.github.com>
2023-04-09 11:43:11 +0300

    initial commit

---
+++ core/urls.py
@@ -0,0 +1,13 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from django.contrib import admin
+from django.urls import path, include 
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path('', include('admin_volt.urls')),
+]
+
